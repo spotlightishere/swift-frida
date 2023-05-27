78cc5295e38a3d66a513417d37529129b2e75311
Spotlight <spotlight@joscomputing.space>
2023-05-26 21:01:59 -0500

    Require macOS 13.0
    Concurrency requires macOS 10.15 and above - let's go ahead and set the latest macOS version (13.0) as the minimum requirement.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Frida",
+    platforms: [
+        .macOS(.v13)
+    ],
     products: [
         .library(
             name: "Frida",
